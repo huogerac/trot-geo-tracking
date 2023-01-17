350804803ad62c6e368832a87ed544f92444270c
tomasrajao <t-tomas.rajao@cnidis.com.br>
2023-01-16 21:05:40 -0300

    Alterada forma de obter atualização da localização

--- public/script.js
+++ public/script.js
@@ -1,10 +1,15 @@
- let h2 = document.querySelector('h2')
+ let lat = document.getElementById('get-lat')
+ let lon = document.getElementById('get-lon')
+ let time = document.getElementById('get-time')
 
-  const geoSuccess = (position) => {
-    h2.textContent = `Latitude: ${position.coords.latitude}\nLongitude: ${position.coords.longitude}`
+  let geoSuccess = (position) => {
+    lat.textContent = `Latitude: ${position.coords.latitude}`
+    lon.textContent = `Longitude: ${position.coords.longitude}`
+    time.textContent = `Tempo: ${new Date().getTime()}`
     }
-  const geoErr = (err) => {
+  let geoErr = (err) => {
     console.log(`Something went wrong: ${err}`)
 }  
-
+let evento = () => {
   navigator.geolocation.getCurrentPosition(geoSuccess, geoErr)
+}
